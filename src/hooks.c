0d641bb0d84aef1e4c55befa896389c29596bb69
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2023-10-31 12:08:33 -0300

    modified:   fractol 	modified:   src/hooks.c 	modified:   src/utils.c

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 20:17:46 by josfelip          #+#    #+#             */
-/*   Updated: 2023/10/30 23:43:25 by josfelip         ###   ########.fr       */
+/*   Updated: 2023/10/31 11:42:49 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,20 @@ void	ft_zoom(double xdelta, double ydelta, void* param)
 	int32_t		w;
 	int32_t		h;
 	t_complex	pointer;
+	t_complex	d;
 	
 	fractal = param;
 	mlx_get_mouse_pos(fractal->mlx, &w, &h);
 	ztrans(&pointer, fractal, w, h);
+	d.x = pointer.x - fractal->b.x;
+	d.y = fractal->b.y - pointer.y;
 	if (ydelta > 0)
 		fractal->axis_len *= 0.90;
 	else if (ydelta < 0)
 		fractal->axis_len *= 1.10;
 	fractal->a = fractal->axis_len / SIZE;
-	fractal->b.x = pointer.x - fractal->axis_len / 2.0;
-	fractal->b.y = pointer.y + fractal->axis_len / 2.0;
+	fractal->b.x += d.x / 10;
+	fractal->b.y -= d.y / 10;
 	printf("w: %d, h: %d, zoom: %f\n", w, h, 1 / fractal->a);
 }
 
@@ -40,12 +43,14 @@ void	ft_joystick(void *param)
 	fractal = param;
 	if (mlx_is_key_down(fractal->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(fractal->mlx);
+	if (mlx_is_key_down(fractal->mlx, MLX_KEY_R))
+		mandelbrot_init(fractal);
 	if (mlx_is_key_down(fractal->mlx, MLX_KEY_UP))
-		fractal->b.y += fractal->a * STEP;
-	if (mlx_is_key_down(fractal->mlx, MLX_KEY_DOWN))
 		fractal->b.y -= fractal->a * STEP;
+	if (mlx_is_key_down(fractal->mlx, MLX_KEY_DOWN))
+		fractal->b.y += fractal->a * STEP;
 	if (mlx_is_key_down(fractal->mlx, MLX_KEY_LEFT))
-		fractal->b.x -= fractal->a * STEP;
-	if (mlx_is_key_down(fractal->mlx, MLX_KEY_RIGHT))
 		fractal->b.x += fractal->a * STEP;
+	if (mlx_is_key_down(fractal->mlx, MLX_KEY_RIGHT))
+		fractal->b.x -= fractal->a * STEP;
 }
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 20:17:59 by josfelip          #+#    #+#             */
-/*   Updated: 2023/10/30 23:01:19 by josfelip         ###   ########.fr       */
+/*   Updated: 2023/10/31 11:59:10 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #include <stdio.h>
 
 /**
-* @brief Transforms the current pixel position into a point on the z domain
+* @brief Transforms the current pixel position into a point on the z domain.
+* The linear transformation uses the line equation form y = a * x + b
 * @param c The complex number which receives the transformed coordinates
 * @param fractal The fractal instance
 * @param w The current pixel's width
@@ -33,8 +34,8 @@ int32_t	ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 
 int32_t	ft_bernstein_poly(uint32_t i, uint32_t iter_max)
 {
-	double		t;
-	int32_t		color;
+	double	t;
+	int32_t	color;
 
 	t = 1.0 * i / iter_max;
 	color = ft_pixel(
